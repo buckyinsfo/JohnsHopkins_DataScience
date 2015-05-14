55e7e23aed191d41b7e393efb8755d5a8b730aad
Timothy Sayre <tim@cachehive.com>
2015-05-14 01:16:21 -0700

    Commit work in progress.  Need to complete creating the return dataframe.

--- us.cachehive.course.r.programming/complete.R
+++ us.cachehive.course.r.programming/complete.R
@@ -1,5 +1,5 @@
 # Week 2 Part 2 Programming Homework Assignment
-# R programming : Add comment
+# R programming
 # Johns Hopkins Bloomberg School of Public Health
 # by Roger D. Peng, PhD, Jeff Leek, PhD, Brian Caffo, PhD
 # 
@@ -30,4 +30,23 @@ complete <- function(directory, id = 1:332) {
 	data_dir <- paste(data_dir, directory, sep="")
 	files <- list.files(data_dir, full.names=TRUE)
 	
+	## Read the data files into a dataframe object
+	df <- data.frame()
+	for (i in 1:length(files)) { 
+		df <- rbind(df, read.csv(files[i]))
+	}
+	
+	#Remove incomplete cases.
+	df <- na.omit(df)
+	
+	## Extract the desired IDs into a separate dataframe object
+	sub_df <- data.frame(Names = c("id", "nobs") )
+	for(j in 1:length(id)) {
+		idx = df["ID"] == id[j]
+		tmp_df <- df[idx,]
+		sub_df <- rbind(sub_df, c(id[j], nrow(tmp_df)))	
+	}
+	
+	print(sub_df)
+	return(sub_df)
 }
