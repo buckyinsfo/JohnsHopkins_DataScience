e3cd63d368ae2d56a295ad93e63c0e9917f8929d
Timothy Sayre <tim@cachehive.com>
2015-05-14 00:27:03 -0700

    Initial function template.

---
+++ us.cachehive.course.r.programming/complete.R
@@ -0,0 +1,33 @@
+# Week 2 Part 2 Programming Homework Assignment
+# R programming : Add comment
+# Johns Hopkins Bloomberg School of Public Health
+# by Roger D. Peng, PhD, Jeff Leek, PhD, Brian Caffo, PhD
+# 
+# Author: tim
+# Function: complete
+###############################################################################
+
+complete <- function(directory, id = 1:332) {
+	## 'directory' is a character vector of length 1 indicating
+	## the location of the CSV files
+	
+	## 'id' is an integer vector indicating the monitor ID numbers
+	## to be used
+	
+	## Return a data frame of the form:
+	## id nobs
+	## 1  117
+	## 2  1041
+	## ...
+	## where 'id' is the monitor ID number and 'nobs' is the
+	## number of complete cases
+	
+	## data sub-directory
+	data_dir<-c("./data/")
+	##data_dir <- c("")
+	
+	## Get the list of files
+	data_dir <- paste(data_dir, directory, sep="")
+	files <- list.files(data_dir, full.names=TRUE)
+	
+}
